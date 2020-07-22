ddb0d5f7b7b6500c052c580e95da82c2201ec2b5
Isaac Reinaldo Pinheiro de Lima <50757880+isaacrpl7@users.noreply.github.com>
2020-07-22 19:59:18 -0300

    Added chartjs
    New chart added and new API. Confirmed cases to each state.

--- covid19/js/script.js
+++ covid19/js/script.js
@@ -4,9 +4,30 @@ const stateH1 = document.querySelector('.state');
 const confirmed = document.querySelector('.confirmed');
 const deaths = document.querySelector('.deaths');
 const suspects = document.querySelector('.suspects');
+const ctx = document.getElementById('myChart');
+
+//ADDING DATES TO THE ARRAY
+Date.prototype.addDays = function(days) {
+    let date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
+
+function getDates(startDate, stopDate) {
+    let dateArray = new Array();
+    let currentDate = startDate;
+    while (currentDate <= stopDate) {
+        dateArray.push(new Date (currentDate));
+        currentDate = currentDate.addDays(1);
+    }
+    return dateArray;
+}
+
+let actualState;
 
 states.forEach(item => {
     item.addEventListener('click',event => {
+        actualState = item.id;
         fetch(`https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/${item.id}`)
             .then(response => response.json())
                 .then(data => {
@@ -17,5 +38,44 @@ states.forEach(item => {
                     info.classList.remove('hide');
                     info.classList.add('visible');
                 });
+        
+        //RETORNAR CASOS CONFIRMADOS
+        async function getData(){
+            let dataArr = new Array();
+            let arrDays = new Array();
+            let response = await fetch(`https://brasil.io/api/dataset/covid19/caso/data/?state=${actualState}&place_type=state`);
+            let data = await response.json();
+            dataArr = data.results.map(item => item.confirmed).reverse();
+            arrDays = data.results.map(item => item.date).reverse();
+            myChart.data.datasets[0].data = dataArr;
+            myChart.data.labels = arrDays;
+            myChart.update();
+            console.log(dataArr,arrDays);
+        }
+        getData().catch(e => {console.log(e)});
+        
     });
+});
+
+let myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: '',
+        datasets: [{
+            label: 'Confirmed cases',
+            data: '',
+            backgroundColor: 'rgba(55, 0, 179, 0.1)',
+            borderColor: 'rgba(55, 0, 179, 1)',
+            borderWidth: 0
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
 });
